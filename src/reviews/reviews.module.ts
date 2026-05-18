8156abb6b0019bac0f6368a5ac927baab48a489b
Shahbaz Khan <shahbaz@gmail.com>
2026-05-18 08:47:53 +0500

    fix: import AuthModule inside ReviewsModule to resolve auth guard dependencies

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [ReviewsController],
   providers: [ReviewsService],
   exports: [ReviewsService],
